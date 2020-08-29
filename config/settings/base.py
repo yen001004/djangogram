2cd4a25b9c45d1d04878357dae7c583814ec4c71
yen001004 <yenn001004@gmail.com>
2020-08-29 15:32:40 +0900

    data models setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db("DATABASE_URL", default="postgres://localhost/djangogram")
+    "default": env.db("DATABASE_URL", default="postgres://postgres:1004@localhost:5432/djangogram")
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
--- djangogram/users/models.py
+++ djangogram/users/models.py
@@ -1,14 +1,27 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    """Default user for djangogram."""
+    GENDER_CHOICES = [
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('C', 'Custom'),
+    ]
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    user_name = models.CharField(blank=True, max_length=255)
+    profile_photo = models.ImageField(blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    phone_number = models.CharField(blank=True, max_length=255)
+    gender = models.CharField(blank=True, choices=GENDER_CHOICES, max_length=255)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         """Get url for user's detail view.
